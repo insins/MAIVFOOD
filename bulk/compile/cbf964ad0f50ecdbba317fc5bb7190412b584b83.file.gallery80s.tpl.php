911eb6e7806947d3069fc2e921c4778834191402
Inez Van Helleputte <starvomiter@gmail.com>
2013-06-22 23:25:45 +0200

    CSS

--- bulk/compile/cbf964ad0f50ecdbba317fc5bb7190412b584b83.file.gallery80s.tpl.php
+++ bulk/compile/cbf964ad0f50ecdbba317fc5bb7190412b584b83.file.gallery80s.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2013-06-22 16:10:05
+<?php /* Smarty version Smarty-3.1.8, created on 2013-06-22 23:05:36
          compiled from "bulk/templates/pages/partials/gallery80s.tpl" */ ?>
 <?php /*%%SmartyHeaderCode:81290276051c5b03de30213-18049666%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
@@ -7,7 +7,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     'cbf964ad0f50ecdbba317fc5bb7190412b584b83' => 
     array (
       0 => 'bulk/templates/pages/partials/gallery80s.tpl',
-      1 => 1371898986,
+      1 => 1371935135,
       2 => 'file',
     ),
   ),
@@ -15,17 +15,123 @@ $_valid = $_smarty_tpl->decodeProperties(array (
   'function' => 
   array (
   ),
-  'has_nocache_code' => false,
   'version' => 'Smarty-3.1.8',
   'unifunc' => 'content_51c5b03de3e4a7_28721281',
+  'has_nocache_code' => false,
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_51c5b03de3e4a7_28721281')) {function content_51c5b03de3e4a7_28721281($_smarty_tpl) {?>    <div id="80s-Banner">
-        <!-- 80s-Banner -->
-        <header>
-            <!-- text van 80s -->
-            <h1>The 80s</h1>
+<?php if ($_valid && !is_callable('content_51c5b03de3e4a7_28721281')) {function content_51c5b03de3e4a7_28721281($_smarty_tpl) {?><section id="banner-80s">
+    <header>
+        <h1>THE PRESENT</h1>
+        <h2>With <span id="rood">contemporary</span> ingredients we&#8217;ll define the <span>future</span>.</h2>
+    </header>
+</section>
 
+<!-- Alle hamburgers -->
+<div id="achtergrond80s">
+
+    <section id="top3">
+        <header>
+            <h1>these burgers have it!</h1>
         </header>
-        <!-- /80s-Banner -->
-    </div>
+
+        <div id="burgers">
+            <!-- Top burger -->
+            <section id="topBurger">
+                <image src="assets/images/50s-top-burger.png" />
+                <div id="voteCountFirst">115</div>
+
+                <p>Ines van helleputte</p>
+                <ul class="actions">
+                    <li> <a href="1">Vote for this burger</a></li>
+                    <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+                </ul>
+
+            </section>
+
+            <!-- Tweede beste burger -->
+           <section id="secondBurger">
+               <image src="assets/images/50s-top-burger.png" />
+               <div id="voteCountSecond">115</div>
+
+               <p>Ines van helleputte</p>
+               <ul class="actions">
+                   <li> <a href="1">Vote for this burger</a></li>
+                   <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+               </ul>
+
+           </section>
+
+            <!-- DERDE burger -->
+             <section id="thirdBurger">
+                 <image src="assets/images/50s-top-burger.png" />
+                 <div id="voteCountThird">115</div>
+
+                 <p>Ines van helleputte</p>
+                 <ul class="actions">
+                     <li> <a href="1">Vote for this burger</a></li>
+                     <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+                 </ul>
+
+             </section>
+            <div class="dummy">&nbsp;</div>
+
+        </div>
+
+    </section>
+
+    <!-- All the burgers -->
+    <section id="allTheBugers00s">
+        <header>
+            <h1>View all the modern burgers</h1>
+        </header>
+
+        <section id="searchField">
+            <form>
+                <div><input type="text" name="search" value="search for a creator"></div>
+            </form>
+        </section>
+
+        <div class="dummy">&nbsp;</div>
+
+        <!-- Alle burger in een rijtje -->
+        <section class="burgerRow">
+            <section class="burgerSummary">
+                <image src="assets/images/50s-overzicht-burger.png" />
+                <div class="voteCount"">115</div>
+
+                <p>Ines van helleputte</p>
+                <ul class="actions">
+                    <li> <a href="1">Vote for this burger</a></li>
+                    <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+                </ul>
+            </section>
+
+            <section class="burgerSummary">
+                <image src="assets/images/50s-overzicht-burger.png" />
+                <div class="voteCount"">115</div>
+
+                <p>Ines van helleputte</p>
+                <ul class="actions">
+                    <li> <a href="1">Vote for this burger</a></li>
+                    <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+                </ul>
+            </section>
+
+            <section class="burgerSummary">
+                <image src="assets/images/50s-overzicht-burger.png" />
+                <div class="voteCount"">115</div>
+
+                <p>Ines van helleputte</p>
+                <ul class="actions">
+                    <li> <a href="1">Vote for this
+                        burger</a></li>
+                    <li> <a href=index.php?page=detail&decade=50s&burgerId=1>View details</a></li>
+                </ul>
+            </section>
+
+        <div class="dummy">&nbsp;</div>
+        <div class="line">&nbsp;</div>
+        </section">
+    </section>
+</div>
 <?php }} ?>
