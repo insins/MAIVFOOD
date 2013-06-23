45c671a32c9236e79e93ec9fc92718d17e0e5198
Inez Van Helleputte <starvomiter@gmail.com>
2013-06-23 23:23:06 +0200

    responsive

---
+++ bulk/compile/6fc1e9c8124881d5de817766906311b5c7d4961f.file.campaign.tpl.php
@@ -0,0 +1,48 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2013-06-23 22:22:52
+         compiled from "bulk/templates/pages/campaign.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:45694323951c7591c849871-60161987%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6fc1e9c8124881d5de817766906311b5c7d4961f' => 
+    array (
+      0 => 'bulk/templates/pages/campaign.tpl',
+      1 => 1371898986,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '45694323951c7591c849871-60161987',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_51c7591c8881d4_80162056',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51c7591c8881d4_80162056')) {function content_51c7591c8881d4_80162056($_smarty_tpl) {?><section id="home-wrapper">
+
+    <!-- HOME HEADER IMAGE -->
+    <section id="home-header">
+        <header>
+            <h1><br/>'s battle of the centuries</h1>
+        </header>
+        <div id="home-burgers">&nbsp;</div>
+    </section>
+
+    <!-- HOME TEXT AND LINKS -->
+    <section id="home-uitleg-wrapper">
+       <section id="home-uitleg"> 
+        <header>
+            <h1>Will you make our new burger?</h1>
+        </header>
+
+        <p>Which <span>decade&#8217;s</span> flavors do you favor most?
+            Let us know by creating this <span>summer&#8217;s burger</span>
+            and <span>win a car</span> suited for that decade.</p>
+
+        <p><a href="index.php?page=create">Watch our video</a>
+            or <a href="index.php?page=gallery">View our gallery</a></p></section>
+    </section>
+
+</section>
+<?php }} ?>
