11b4fe9a71a22bf8d78b7b6cf3c136ac1733ab49
Inez Van Helleputte <starvomiter@gmail.com>
2013-06-24 20:29:21 +0200

    e

---
+++ bulk/compile/6fc1e9c8124881d5de817766906311b5c7d4961f.file.campaign.tpl.php
@@ -0,0 +1,48 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2013-06-24 20:24:23
+         compiled from "bulk/templates/pages/campaign.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:143006558251c88ed7ef7c07-80761139%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6fc1e9c8124881d5de817766906311b5c7d4961f' => 
+    array (
+      0 => 'bulk/templates/pages/campaign.tpl',
+      1 => 1371898986,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '143006558251c88ed7ef7c07-80761139',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_51c88ed7f026e7_33411529',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51c88ed7f026e7_33411529')) {function content_51c88ed7f026e7_33411529($_smarty_tpl) {?><section id="home-wrapper">
+
+    <!-- HOME HEADER IMAGE -->
+    <section id="home-header">
+        <header>
+            <h1><br/>'s battle of the centuries</h1>
+        </header>
+        <div id="home-burgers">&nbsp;</div>
+    </section>
+
+    <!-- HOME TEXT AND LINKS -->
+    <section id="home-uitleg-wrapper">
+       <section id="home-uitleg"> 
+        <header>
+            <h1>Will you make our new burger?</h1>
+        </header>
+
+        <p>Which <span>decade&#8217;s</span> flavors do you favor most?
+            Let us know by creating this <span>summer&#8217;s burger</span>
+            and <span>win a car</span> suited for that decade.</p>
+
+        <p><a href="index.php?page=create">Watch our video</a>
+            or <a href="index.php?page=gallery">View our gallery</a></p></section>
+    </section>
+
+</section>
+<?php }} ?>
---
+++ bulk/compile/8440fad56a6e193cbd984eebe94c35d3fb16dd17.file.create.tpl.php
@@ -0,0 +1,87 @@
+<?php /* Smarty version Smarty-3.1.8, created on 2013-06-24 20:24:27
+         compiled from "bulk/templates/pages/create.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:188948484751c88edb60fc05-16217085%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '8440fad56a6e193cbd984eebe94c35d3fb16dd17' => 
+    array (
+      0 => 'bulk/templates/pages/create.tpl',
+      1 => 1372083233,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '188948484751c88edb60fc05-16217085',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_51c88edb66a125_41524175',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_51c88edb66a125_41524175')) {function content_51c88edb66a125_41524175($_smarty_tpl) {?><!-- create a burger -->
+
+    <!-- MAKE THE BEST BURGER
+    AND WIN A CAR -->
+    <section id="car-header-wrapper">
+        <section id="car-header">
+            <header>
+            <h1>Create the best burger of all time</h1>
+            <h2>And win a classic car</h2>
+        </header>
+        <div id="car-iphone-image">&nbsp;</div>
+        </section>
+    </section>
+
+    <!-- HOW DOES IT WORK + LINK APP STORE -->
+    <section id="how-does-it-work-wrapper">
+
+        <section id="how-does-it-work">
+        <header>
+            <h1>How does it work?</h1>
+        </header>
+
+        <div class="text-wrapper-does-it-work">
+            <p> Don&#8217;t worry, it&#8217;s really easy!</p>
+            <ol>
+                <li>Download our app from the app store</li>
+                <li>Log in on facebook and create your burger</li>
+                <li>View your burger in the gallery</li>
+                <li>Get the most votes</li>
+                <li>Win a car!</li>
+            </ol>
+
+            <!-- App store button -->
+            <p><a href="http://appstore.com">Download from app store</a></p>
+
+        </div>
+        <!-- Image -->
+        <div id="flamingo-iphone-image">&nbsp;</div>
+        
+    </section>
+        
+    </section>
+
+
+    <!-- FILMPJE -->
+    <section id="video-content">
+            <header>
+                <h1>Check out our promovideo</h1>
+                <h2>It's super awesome</h2>
+            </header>
+
+         <div id="video-wrapper">
+
+
+            <video id="video-player" width="944" height="566" controls>
+              <source src="assets/video/movie.mp4" type="video/mp4">
+              <source src="assets/video/movie.ogg" type="video/ogg">
+                <source src="assets/video/movie.webm" type="video/webm">
+                Your browser does not support the video tag.
+            </video>
+
+             <div id="surfplank">&nbsp;</div>
+             <div id="ijsje">&nbsp;</div>
+
+         </div>
+    </section><?php }} ?>
